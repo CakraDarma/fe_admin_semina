8f46d849a7ba434887475dbd9796b6e4252fc6d3
CakraDarma <baguscakraa02@gmail.com>
2023-01-16 01:13:36 +0800

    add talents

--- src/redux/talents/actions.js
+++ src/redux/talents/actions.js
@@ -1,8 +1,8 @@
 import {
-  START_FETCHING_TALENTS,
-  SUCCESS_FETCHING_TALENTS,
-  ERROR_FETCHING_TALENTS,
-  SET_KEYWORD,
+	START_FETCHING_TALENTS,
+	SUCCESS_FETCHING_TALENTS,
+	ERROR_FETCHING_TALENTS,
+	SET_KEYWORD,
 } from './constants';
 
 import { getData } from '../../utils/fetch';
@@ -12,57 +12,59 @@ import { clearNotif } from '../notif/actions';
 let debouncedFetchTalents = debounce(getData, 1000);
 
 export const startFetchingTalents = () => {
-  return {
-    type: START_FETCHING_TALENTS,
-  };
+	return {
+		type: START_FETCHING_TALENTS,
+	};
 };
 
 export const successFetchingTalents = ({ talents }) => {
-  return {
-    type: SUCCESS_FETCHING_TALENTS,
-    talents,
-  };
+	return {
+		type: SUCCESS_FETCHING_TALENTS,
+		talents,
+	};
 };
 
 export const errorFetchingTalents = () => {
-  return {
-    type: ERROR_FETCHING_TALENTS,
-  };
+	return {
+		type: ERROR_FETCHING_TALENTS,
+	};
 };
 
 export const fetchTalents = () => {
-  return async (dispatch, getState) => {
-    dispatch(startFetchingTalents());
+	return async (dispatch, getState) => {
+		dispatch(startFetchingTalents());
 
-    try {
-      setTimeout(() => {
-        dispatch(clearNotif());
-      }, 5000);
+		try {
+			setTimeout(() => {
+				dispatch(clearNotif());
+			}, 5000);
 
-      let params = {
-        keyword: getState().talents.keyword,
-      };
+			let params = {
+				keyword: getState().talents.keyword,
+			};
+			// console.log(params);
+			// keyword: 'f'
 
-      let res = await debouncedFetchTalents('/cms/talents', params);
+			let res = await debouncedFetchTalents('/cms/talents', params);
 
-      res.data.data.forEach((res) => {
-        res.avatar = res.image.name;
-      });
+			res.data.data.forEach((res) => {
+				res.avatar = res.image.name;
+			});
 
-      dispatch(
-        successFetchingTalents({
-          talents: res.data.data,
-        })
-      );
-    } catch (error) {
-      dispatch(errorFetchingTalents());
-    }
-  };
+			dispatch(
+				successFetchingTalents({
+					talents: res.data.data,
+				})
+			);
+		} catch (error) {
+			dispatch(errorFetchingTalents());
+		}
+	};
 };
 
 export const setKeyword = (keyword) => {
-  return {
-    type: SET_KEYWORD,
-    keyword,
-  };
+	return {
+		type: SET_KEYWORD,
+		keyword,
+	};
 };
